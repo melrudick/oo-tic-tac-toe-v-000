503e0c9d17aa22d298ec9a6cde2d0bdb07829769
Melissa Rudick <mel.madrugada@gmail.com>
2017-09-05 16:42:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,7 +67,7 @@ class TicTacToe
     def current_player
       turn_count.even? ? "X" : "O"
     end
-
+end
 #     def turn
 #       puts "Please enter 1-9:"
 #       integer = gets.strip
@@ -79,26 +79,26 @@ class TicTacToe
 #         turn
 #       end
 #       display_board
-# 
+#
 #     end
-# 
+#
 #     def won?
 #       WIN_COMBINATIONS.each do |win_combination|
 #         win_index_1 = win_combination[0]
 #         win_index_2 = win_combination[1]
 #         win_index_3 = win_combination[2]
-# 
+#
 #         position_1 = @board[win_index_1]
 #         position_2 = @board[win_index_2]
 #         position_3 = @board[win_index_3]
-# 
+#
 #         if position_1 == "X" && position_2 == "X" && position_3 == "X"
 #           return win_combination # return the win_combination indexes that won.
 #         elsif  position_1 == "O" && position_2 == "O" && position_3 == "O"
 #           return win_combination
 #     end
 # end
-# 
+#
 #   def full?
 #     @board.all? do |pos|
 #       pos == "X" || pos == "O"
