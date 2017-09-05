0a5504c53ad3f802a3bcc5d78df47969a97dbeb9
Melissa Rudick <mel.madrugada@gmail.com>
2017-09-05 16:41:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,40 +68,40 @@ class TicTacToe
       turn_count.even? ? "X" : "O"
     end
 
-    def turn
-      puts "Please enter 1-9:"
-      integer = gets.strip
-      position = input_to_index(integer)
-      if valid_move?(position)
-        char = current_player
-        move(position, char)
-      else
-        turn
-      end
-      display_board
-
-    end
-
-    def won?
-      WIN_COMBINATIONS.each do |win_combination|
-        win_index_1 = win_combination[0]
-        win_index_2 = win_combination[1]
-        win_index_3 = win_combination[2]
-
-        position_1 = @board[win_index_1]
-        position_2 = @board[win_index_2]
-        position_3 = @board[win_index_3]
-
-        if position_1 == "X" && position_2 == "X" && position_3 == "X"
-          return win_combination # return the win_combination indexes that won.
-        elsif  position_1 == "O" && position_2 == "O" && position_3 == "O"
-          return win_combination
-    end
-end
-
-  def full?
-    @board.all? do |pos|
-      pos == "X" || pos == "O"
+#     def turn
+#       puts "Please enter 1-9:"
+#       integer = gets.strip
+#       position = input_to_index(integer)
+#       if valid_move?(position)
+#         char = current_player
+#         move(position, char)
+#       else
+#         turn
+#       end
+#       display_board
+# 
+#     end
+# 
+#     def won?
+#       WIN_COMBINATIONS.each do |win_combination|
+#         win_index_1 = win_combination[0]
+#         win_index_2 = win_combination[1]
+#         win_index_3 = win_combination[2]
+# 
+#         position_1 = @board[win_index_1]
+#         position_2 = @board[win_index_2]
+#         position_3 = @board[win_index_3]
+# 
+#         if position_1 == "X" && position_2 == "X" && position_3 == "X"
+#           return win_combination # return the win_combination indexes that won.
+#         elsif  position_1 == "O" && position_2 == "O" && position_3 == "O"
+#           return win_combination
+#     end
+# end
+# 
+#   def full?
+#     @board.all? do |pos|
+#       pos == "X" || pos == "O"
 
   end
 end
